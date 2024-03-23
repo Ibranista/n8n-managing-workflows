6dbb59447d79b31a2987b10149aa12f3aa568ce5
ibrahim <ibrahim@addissoftware.com>
2024-03-24 00:29:42 +0300

    ft: swagger doc.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Learning type orm')
+    .setDescription('Performing CRUD operations using typeorm and nestjs')
+    .setVersion('1.0')
+    .addTag('learning')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
